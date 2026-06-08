fa4b4f33d5a45b3b884359564208c508e1bbfa23
PhodzoDev <PhodzoW0l7>
2026-06-08 16:12:35 +0200

    Created SupabaseApp with Vite

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <>
+    <h1 className="text-center pt-4 text-2xl font-bold">React Supabase App</h1>
+    <RouterProvider router={router}></RouterProvider>
+  </>
+  </StrictMode>,
+);
